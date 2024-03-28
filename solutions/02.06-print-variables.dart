682cae6f9dbf52d71130d10b9580a500366df7aa
Abdul Basit <98378149+abdullbasit1@users.noreply.github.com>
2024-03-28 07:09:29 +0500

    .gitignore file added and first solution done for exercise 1

---
+++ solutions/02.06-print-variables.dart
@@ -0,0 +1,11 @@
+void main() {
+  String firstName = "Abdul";
+  String lastName = "Basit";
+  int age = 19;
+  double height = 1.67;
+
+  print(firstName);
+  print(lastName);
+  print(age);
+  print(height);
+}
